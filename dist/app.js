277dfdd7c0841488c8554fde1a2fc754184356d9
tzvir <tzvi.ra@gmail.com>
2026-01-13 10:38:19 +0200

    Add Docker debugging support with VS Code breakpoints

---
+++ dist/app.js
@@ -0,0 +1,12 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello from Node.js Docker boilerplate with TypeScript' });
+});
+exports.default = app;
+//# sourceMappingURL=app.js.map
